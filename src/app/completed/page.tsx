69498618d2e53c05e7951ac490538d2fa03ee978
kuru-shu <kurushu71@gmail.com>
2024-08-01 00:43:02 +0900

    change directory

--- src/app/completed/page.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function Completed() {
-  return <div>完了リスト</div>;
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+import CompletedList from '@/components/completed/completedList';
+import AddTodo from '@/components/todo/addTodo';
+import TodoList from '@/components/todo/todoList';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+export default function Home() {
+  return (
+    <div>
+      <Tabs defaultValue="TODO _LIST" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="TODO_LIST">TODO</TabsTrigger>
+          <TabsTrigger value="COMPLETED_LIST">COMPLETED</TabsTrigger>
+        </TabsList>
+        <TabsContent value="TODO_LIST">
+          <Card>
+            <CardHeader>
+              <CardTitle>TODOリスト</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <TodoList />
+              <AddTodo />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="COMPLETED_LIST">
+          <Card>
+            <CardHeader>
+              <CardTitle>完了リスト</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <CompletedList />
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
